688af19efc7b4f6c9f91020c7d90923cb3789ba3
edwingarciagaitan82 <edwingarciagaitan82@gmail.com>
2024-09-11 20:25:02 -0500

    EDU dot env

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
-// const URIDB = "mongodb+srv://edwingarcia2022:12345@cluster0.d9a6c.mongodb.net/ecommerce?retryWrites=true&w=majority";
-const URIDB = "mongodb://127.0.0.1:27017/ecommerce";
+require('dotenv').config();
+console.log("uridb",process.env.URIDB)
+const URIDB = process.env.URIDB
 const connectDB = async() =>{
     options = {
         // keepAlive : true,
